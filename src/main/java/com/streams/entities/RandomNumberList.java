d6886abaa23597a65de416307e2dd450baee5ee0
Naveen <naveen.mca2019@gmail.com>
2026-02-11 21:51:31 +0530

    Entity Class

---
+++ src/main/java/com/streams/entities/RandomNumberList.java
@@ -0,0 +1,20 @@
+package com.streams.entities;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class RandomNumberList {
+    static Random random = new Random();
+
+    public static List<Integer> getRandomNumberList(int count, int origin, int bound) {
+        return random.ints(count, origin, bound)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+    public static List<Integer> getRandomNumberList(int count) {
+        return random.ints(count,1,100)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
