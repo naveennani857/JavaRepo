05065ae1870060b3c0098552ccf8dc44326ada96
Naveen <naveen.mca2019@gmail.com>
2026-02-11 21:18:21 +0530

    Stream programs

---
+++ src/main/java/com/streams/streamWithNumbers/SumOfNumbersInList.java
@@ -0,0 +1,17 @@
+package com.streams.streamWithNumbers;
+import com.streams.entities.RandomNumberList;
+
+import java.util.*;
+
+public class SumOfNumbersInList {
+    public static void main(String[] args) {
+        List<Integer> nums = RandomNumberList.getRandomNumberList(10,1,10);
+        System.out.println("List of Numbers : "+nums);
+
+        //Optional<Integer> sum = nums.stream().reduce((x,y) -> x+y);   //with Lambda
+        Optional<Integer> sum = nums.stream().reduce(Integer::sum);     //with method Reference
+
+        System.out.println(sum.get());
+
+    }
+}
